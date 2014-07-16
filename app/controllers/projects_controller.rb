894130c103a83cb06ea5bb531ab84387a64f1774
jdcc <jclark@cyber.law.harvard.edu>
2014-07-16 11:01:08 -0400

    style tweaks

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,14 @@
 class ProjectsController < ApplicationController
-  before_action :load_project, only: [:show]
+  before_action :load_project, only: [:show, :edit]
 
   def index
     @projects = current_user.projects
   end
 
   def show
+  end
+
+  def edit
   end
 
   private
