7e5fb90b308bad154dc15a1ed736964cd4201155
Dave Mayo <dave_mayo@harvard.edu>
2015-08-14 12:16:42 -0400

    Add websocket_url to production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ LinkOMatic::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.websocket_url = "#{ROOT_URL}:3000/websocket"
 end
